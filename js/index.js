4309821b3510921242da2a73b4ca1897fa1f465c
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-09-29 12:22:46 -0600

    mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// Your code goes here
+// mouseover
+const navBar = document.querySelector('.main-navigation')
+navBar.addEventListener('mouseover', function(event){
+    event.target.style.backgroundColor = 'purple'
+})
+
+// keydown
+
+// wheel
+
+// load 
+
+// focus
+
+// resize 
+
+// scroll
+
+// select
+
+// dbclick
+
+// drag and drop
